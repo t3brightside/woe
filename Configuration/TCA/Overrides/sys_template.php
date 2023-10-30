3f832cfdab61e1bd9e73defb7251f2bd3695332f
Tanel Põld <tanel@brightside.ee>
2023-10-30 16:24:48 +0200

    [TASK] Static TS templates for pages

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -3,6 +3,12 @@ use \TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 ExtensionManagementUtility::addStaticFile(
     'woe',
-    'Configuration/TypoScript/',
-    'Woe'
+    'Configuration/TypoScript/SpotDetail/',
+    'WOE - Spot Detail'
+);
+
+ExtensionManagementUtility::addStaticFile(
+    'woe',
+    'Configuration/TypoScript/SpotList/',
+    'WOE - JSON Spot List'
 );
